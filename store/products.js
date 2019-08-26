b55f30e66042be40e9947d78494b3ed1b5968815
Pierre GOLFIER <pgolfier.pro@gmail.com>
2019-08-26 20:44:32 +0200

    edit products images url

--- store/products.js
+++ store/products.js
@@ -4,18 +4,21 @@ export const state = () => ({
       {
         id: 1,
         name: "Table 1",
+        image: "/pictures/table-1.jpg",
         price: 149.95,
         description: ""
       },
       {
         id: 2,
         name: "Table 2",
+        image: "/pictures/table-2.jpg",
         price: 189.95,
         description: ""
       },
       {
         id: 3,
         name: "Table 3",
+        image: "/pictures/table-3.jpg",
         price: 299.95,
         description: ""
       }
@@ -24,18 +27,21 @@ export const state = () => ({
       {
         id: 1,
         name: "Chair 1",
+        image: "/pictures/chair-1.jpg",
         price: 99.95,
         description: ""
       },
       {
         id: 2,
         name: "Chair 2",
+        image: "/pictures/chair-2.jpg",
         price: 159.95,
         description: ""
       },
       {
         id: 3,
         name: "Chair 3",
+        image: "/pictures/chair-3.jpg",
         price: 199.95,
         description: ""
       }
