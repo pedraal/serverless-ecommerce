9fc5cd0eb2457a5ce414b8f3b98cbbfccb5e4c8f
Pierre GOLFIER <pgolfier.pro@gmail.com>
2019-08-27 03:42:29 +0200

    added a rating ui package and made small edit on layout

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -9,12 +9,22 @@
       <div class="level">
         <div class="level-item">
           <div class="has-text-centered">
-            <p class="title is-4">{{product.name}}</p>
-            <p class="subtitle is-6">{{product.price}}€</p>
+            <p class="title is-4 is-uppercase has-text-weight-light">{{product.name}}</p>
+            <p class="subtitle is-6 has-text-weight-bold">{{product.price}}€</p>
           </div>
         </div>
         <div class="level-item">
-          <button class="button is-primary">
+          <star-rating
+            :rating="product.rating"
+            active-color="hsl(171, 100%, 41%)"
+            :star-size="15"
+            :read-only="true"
+            :show-rating="false"
+            style="margin: 5px 0"
+          ></star-rating>
+        </div>
+        <div class="level-item">
+          <button class="button is-outlined is-primary">
             <span class="icon">
               <i class="fas fa-search"></i>
             </span>
@@ -27,13 +37,19 @@
 </template>
 
 <script>
+import StarRating from "vue-star-rating";
 export default {
+  components: { StarRating },
   props: ["product"]
 };
 </script>
 
 <style lang="scss" scoped>
+.title {
+  font-family: "Montserrat", sans-serif;
+}
 .subtitle {
+  font-family: "Lexend Tera", sans-serif;
   margin-top: 0 !important;
 }
 </style>
--- data/products.js
+++ data/products.js
@@ -4,19 +4,22 @@ export default {
       id: 1,
       name: "norberg",
       image: "/pictures/table-1.jpg",
-      price: 149.95
+      price: 149.95,
+      rating: 5
     },
     {
       id: 2,
       name: "stornäs",
       image: "/pictures/table-2.jpg",
-      price: 189.95
+      price: 189.95,
+      rating: 4
     },
     {
       id: 3,
       name: "ekedalen",
       image: "/pictures/table-3.jpg",
-      price: 299.95
+      price: 299.95,
+      rating: 4
     }
   ],
   chairs: [
@@ -24,19 +27,22 @@ export default {
       id: 1,
       name: "stig",
       image: "/pictures/chair-1.jpg",
-      price: 99.95
+      price: 99.95,
+      rating: 4
     },
     {
       id: 2,
       name: "dalfred",
       image: "/pictures/chair-2.jpg",
-      price: 159.95
+      price: 159.95,
+      rating: 4
     },
     {
       id: 3,
       name: "henriksdal",
       image: "/pictures/chair-3.jpg",
-      price: 199.95
+      price: 199.95,
+      rating: 5
     }
   ]
 }
