ee52bca79a026b8cbe05967aa690a03d465addc3
Pierre GOLFIER <pgolfier.pro@gmail.com>
2019-08-27 04:23:36 +0200

    fix responsive for ProductCard comp

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -6,24 +6,24 @@
       </figure>
     </div>
     <div class="card-content">
-      <div class="level">
-        <div class="level-item">
+      <div class="columns is-multiline is-vcentered is-centered">
+        <div class="column is-6-tablet is-4-widescreen">
           <div class="has-text-centered">
             <p class="title is-4 is-uppercase has-text-weight-light">{{product.name}}</p>
             <p class="subtitle is-6 has-text-weight-bold">{{product.price}}€</p>
           </div>
         </div>
-        <div class="level-item">
+        <div class="column is-12-tablet is-4-widescreen">
           <star-rating
             :rating="product.rating"
             active-color="hsl(171, 100%, 41%)"
             :star-size="15"
             :read-only="true"
             :show-rating="false"
-            style="margin: 5px 0"
+            style="margin: 5px auto; width: 80px !important;"
           ></star-rating>
         </div>
-        <div class="level-item">
+        <div class="column is-12-tablet is-4-widescreen has-text-centered">
           <button class="button is-outlined is-primary">
             <span class="icon">
               <i class="fas fa-search"></i>
