0209eb32d431539fa8e2f488a111320b68902bed
Pierre GOLFIER <pgolfier.pro@gmail.com>
2019-09-10 14:33:04 +0200

    refactored Header's layout using CSS Grid

--- components/Header copy.vue
+++ components/Header copy.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class="hero container">
-      <div id="design">Design</div>
-      <div id="unique">Unique</div>
-      <div id="sober">Sober</div>
-      <div id="comfort" class="has-font-light">Comfort</div>
+      <span id="design">Design</span>
+      <span id="unique">Unique</span>
+      <span id="sober">Sober</span>
+      <span id="comfort" class="has-font-light">Comfort</span>
     </div>
     <Nav />
   </div>
@@ -21,60 +21,56 @@ export default {
 
 <style lang="scss" scoped>
 .hero {
+  position: relative;
   background: url("/pictures/hero-2.jpg");
   background-position: center;
   background-repeat: no-repeat;
   padding-bottom: 2rem;
-  display: grid;
-  grid-template-columns: 50px 150px 110px 1fr;
-  grid-template-rows: 100px 100px 100px;
-  div {
+  span {
+    position: relative;
+    display: block;
     font-size: 4rem;
-
     &#design {
+      position: absolute;
+      bottom: 0;
       font-size: 10rem;
       color: #9c9898;
       font-family: "Parisienne", cursive;
-      grid-area: 3/1/4/5;
-      place-self: end stretch;
     }
     &#unique {
+      margin-left: 10rem;
       font-family: "Montserrat", sans-serif;
-      grid-area: 1/3/2/5;
     }
     &#sober {
+      margin-left: 5rem;
       font-family: "Open Sans Condensed", sans-serif;
-      grid-area: 2/2/3/5;
     }
     &#comfort {
+      margin-left: 13rem;
       font-family: "Lexend Tera", sans-serif;
-      grid-area: 3/4/4/5;
     }
   }
 }
 
 @media screen and (max-width: 769px) {
   .hero {
-    background-size: auto 100%;
-    background-position: center center;
-    grid-template-rows: 75px 75px 75px;
-    grid-template-columns: 25px 75px 10px 1fr;
-    div {
-      font-size: 3rem;
+    padding-bottom: 150px;
+    background-size: 100%;
+    background-position: center bottom;
+    span {
+      font-size: 2rem;
       &#design {
         font-size: 8rem;
+        top: 0;
       }
-    }
-  }
-}
-
-@media screen and (max-width: 440px) {
-  .hero {
-    grid-template-columns: 25px 25px 20px 1fr;
-    div {
-      font-size: 3rem;
-      &#design {
-        font-size: 8rem;
+      &#unique {
+        margin-left: 5rem;
+      }
+      &#sober {
+        margin-left: 2rem;
+      }
+      &#comfort {
+        margin-left: 7rem;
       }
     }
   }
